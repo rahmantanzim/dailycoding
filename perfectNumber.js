225584cb1053ac3393b954c49b5582d53c58acd3
Tanzim <tanzim008@gmail.com>
2025-03-03 00:29:44 -0330

    probme solved!

--- perfectNumber.js
+++ perfectNumber.js
@@ -4,3 +4,23 @@
 
 // For example, given 1, you should return 19. Given 2, you should return 28.
 
+// SOLUTION
+// *****************
+
+const findPerfectNumber = (n)=>{
+    let count = 0;
+    let num = 18; 
+    while(count < n){
+        num++;
+        if(findSumOfDigits(num) === 10){
+            count++;
+        }
+    }
+    return num;
+    function findSumOfDigits(num){
+        return num.toString().split('').reduce((sum,digit)=> sum + parseInt(digit),0);
+    }
+}
+
+console.log(findPerfectNumber(1)); //should return 19
+console.log(findPerfectNumber(2)); //should return 19
